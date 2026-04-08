8f08826a9c6fb10ed19734a47eeaae623fc36409
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-08 11:43:29 +0530

    Update SumExceptSelf.java

--- src/SumExceptSelf.java
+++ src/SumExceptSelf.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class SumExceptSelf {
-   
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Calculate sum of all elements (Total Sum)
-        // TODO: For each element, print (Total Sum - element)
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read number of elements
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        int totalSum = 0;
+
+        // Read array elements and calculate total sum
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+            totalSum += nums[i];
+        }
+
+        // Compute and print result
+        for (int i = 0; i < n; i++) {
+            System.out.print((totalSum - nums[i]) + " ");
+        }
+
+        sc.close();
+    }
 }
